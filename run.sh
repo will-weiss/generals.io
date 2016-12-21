6c72f07cf13da7d0d8832d9f375a909a04390411
will-weiss <will.weiss1230@gmail.com>
2016-12-21 09:28:11 -0500

    add run.sh

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+start_selenium_server() {
+  java -jar -Dwebdriver.geckodriver.driver=./geckodriver selenium-server-standalone-3.0.1.jar &
+}
+
+run() {
+  start_selenium_server && npm run compile && npm start
+}
+
+run
