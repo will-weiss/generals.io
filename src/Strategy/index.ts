f3739966875f78358bc2d5de971a978c54274f44
will-weiss <will.weiss1230@gmail.com>
2016-12-21 20:12:19 -0500

    separate getRandomOrders

---
+++ src/Strategy/index.ts
@@ -0,0 +1,18 @@
+import { sample } from 'lodash'
+import { Tile, Order, GameState, GameConfiguration } from '../types'
+
+
+export function getRandomOrders( gameConfiguration: GameConfiguration, gameState: GameState): Order[] {
+  const myArmies = gameState.armies.get(gameConfiguration.revealed.myColor)!
+  const orders: Order[] = []
+
+  for (const [from, armySize] of myArmies.entries()) {
+    if (armySize < 1) continue
+    const to: Tile | undefined = sample(Array.from(gameConfiguration.revealed.adjacencies.get(from)!))
+    if (!to) continue
+    orders.push({ from, to, splitArmy: Math.random() < 0.5 })
+    if (orders.length > 1) break
+  }
+
+  return orders
+}
--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,17 @@
-import { sample } from 'lodash'
 import BrowserGame from './BrowserGame'
 import GameConfiguration from './GameConfiguration'
 import createGameState from './GameState'
-import { Tile, Order, GameState } from './types'
+import { getRandomOrders } from './Strategy'
+import { Order, GameState } from './types'
 
 
 function main(): void {
-
   const connection = new BrowserGame()
   let gameConfiguration: GameConfiguration
   let gameState: GameState
 
-  function takeRandomTurn(): Promise<void> {
-
-    if (gameState.gameOver) return Promise.resolve()
-    const myArmies = gameState.armies.get(gameConfiguration.revealed.myColor)!
-    const orders: Order[] = []
-
-    for (const [from, armySize] of myArmies.entries()) {
-      if (armySize < 1) continue
-      const to: Tile | undefined = sample(Array.from(gameConfiguration.revealed.adjacencies.get(from)!))
-      if (!to) continue
-      orders.push({ from, to, splitArmy: Math.random() < 0.5 })
-      if (orders.length > 1) break
-    }
-
+  function takeTurn(): Promise<void> {
+    const orders: Order[] = getRandomOrders(gameConfiguration, gameState)
     return connection.submitOrders(orders)
   }
 
@@ -32,13 +19,13 @@ function main(): void {
     console.log('start', state.turn)
     gameConfiguration = new GameConfiguration('Anonymous', state)
     gameState = createGameState(gameConfiguration, state)
-    takeRandomTurn()
+    takeTurn()
   })
 
   connection.on('nextTurn', state => {
     console.log('nextTurn', state.turn)
     gameState = createGameState(gameConfiguration, state, gameState)
-    takeRandomTurn()
+    takeTurn()
   })
 
   connection.on('gameOver', state => console.log('gameOver', state))
