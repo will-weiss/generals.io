096f7c56b583aa75aeac9f379f5e475547dd3a0c
will-weiss <will.weiss1230@gmail.com>
2017-01-16 15:25:28 -0500

    bot has random name

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,8 @@
-export const botName = 'dookiebot'
+import { sample } from 'lodash'
+
+const smallWords = require('fs').readFileSync('/usr/share/dict/words', 'utf8').split('\n').filter(word => word.length < 8)
+
+export const botName = sample(smallWords) + '-' + sample(smallWords)
 export const generalsIoUrl = 'http://generals.io'
 export const webdriverOpts = { desiredCapabilities: { browserName: 'chrome' } }
 export const viewportSize = { height: 4000, width: 4000 }
